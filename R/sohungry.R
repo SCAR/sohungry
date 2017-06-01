119ccd354f44f6abccb7c58201b7d80e7c8acd64
Ben Raymond <ben.raymond@aad.gov.au>
2017-06-01 22:27:10 +1000

    update imports

--- R/sohungry.R
+++ R/sohungry.R
@@ -5,7 +5,7 @@
 #' @references \url{http://data.aad.gov.au/trophic}
 # @importFrom aadcdb db_close db_open db_query
 #' @importFrom assertthat assert_that is.flag is.number is.string
-#' @importFrom curl curl_download
+#' @importFrom curl curl_download handle_setopt new_handle
 #' @importFrom dplyr %>% group_by_ left_join mutate_ filter_ rename_ select_ summarize_
 #' @importFrom lubridate ymd_hms
 #' @importFrom readr read_csv
