861b4c33c14280db90fa3fae8bc8a8f4ed3cc318
Ben Raymond <ben.raymond@aad.gov.au>
2017-07-28 14:40:14 +1000

    aphia_id to taxon_aphia_id

--- R/allometry.R
+++ R/allometry.R
@@ -3,12 +3,12 @@ globalVariables("allometry_data") # To make R CMD Check happy
 #' Allometric equations for cephalopods
 #'
 #' Estimate body size measurements (currently mantle length and/or body mass) from lower rostral length.
-#' Either the taxon name or the aphia ID must be provided.
+#' Either the taxon name or the Aphia ID must be provided.
 #' Note that some allometric equations apply at the genus- or family-level. Currently these won't be found if a species
 #' name or ID is provided here. Refer to \code{\link{allometry_data}} to see all available equations.
 #'
 #' @param taxon_name string: the name of the taxon
-#' @param aphia_id numeric: the aphia ID of the taxon
+#' @param taxon_aphia_id numeric: the Aphia ID of the taxon
 #' @param LRL numeric: one or more lower rostral lengths (in mm)
 #'
 #' @return list of data.frames, one per supplied lower rostral length value
@@ -20,13 +20,13 @@ globalVariables("allometry_data") # To make R CMD Check happy
 #'   LRL=c(13.9,11.3))
 #'
 #' @export
-so_allometry_cephalopods <- function(taxon_name,aphia_id,LRL) {
+so_allometry_cephalopods <- function(taxon_name,taxon_aphia_id,LRL) {
     if (!missing(taxon_name)) {
         idx <- allometry_data$taxon_name==taxon_name
-    } else if (!missing(aphia_id)) {
-        idx <- allometry_data$aphia_id==aphia_id
+    } else if (!missing(taxon_aphia_id)) {
+        idx <- allometry_data$taxon_aphia_id==taxon_aphia_id
     } else {
-        stop("need taxon_name or aphia_id")
+        stop("need taxon_name or taxon_aphia_id")
     }
     idx <- which(idx & allometry_data$input_measurement=="lower rostral length")
     lapply(LRL,function(z) {
