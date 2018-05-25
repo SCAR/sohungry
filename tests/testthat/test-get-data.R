d9e3771b4421b173352ef0a28e179ce6495d08cb
Ben Raymond <ben.raymond@aad.gov.au>
2018-05-25 15:34:45 +1000

    re-enable skipped test

--- tests/testthat/test-get-data.R
+++ tests/testthat/test-get-data.R
@@ -3,8 +3,7 @@ context("sohungry data retrieval")
 test_that("data retrieval works", {
     expect_warning(xi <- so_isotopes(method="get"))
     expect_warning(xi <- so_isotopes(method="get", format="wide"))
-    xd <- so_diet(method="get")
-
-    skip("skipping isotopes/mv test temporarily")
     xi <- so_isotopes(method="get", format="mv")
+
+    xd <- so_diet(method="get")
 })
