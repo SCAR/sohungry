800fb3308e0b7475025908d30de3c502bb56026c
Ben Raymond <ben.raymond@aad.gov.au>
2018-08-10 15:00:17 +1000

    custom CITATION file

--- R/helpers.R
+++ R/helpers.R
@@ -6,5 +6,5 @@ so_set_opt <- function(...) {
     opts <- so_opts()
     newopts <- list(...)
     for (nm in names(newopts)) opts[[nm]] <- newopts[[nm]]
-    options(list(sohungry=opts))
+    options(list(sohungry = opts))
 }
