8100e695352af81cb27742f5ee39cb42c85c5f13
Ben Raymond <ben.raymond@aad.gov.au>
2021-04-01 13:42:18 +1100

    urls

--- R/sohungry.R
+++ R/sohungry.R
@@ -4,7 +4,7 @@
 #'
 #' @name sohungry
 #' @docType package
-#' @references \url{http://data.aad.gov.au/trophic}
+#' @references \url{https://scar.org/resources/southern-ocean-diet-energetics/}
 #' @importFrom assertthat assert_that is.flag is.number is.string
 #' @importFrom curl curl_download handle_setopt handle_setheaders new_handle
 #' @importFrom dplyr %>% bind_rows bind_cols group_by left_join mutate rename summarize tibble
