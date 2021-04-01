35f9334ab78156c937a014231adc16f4ea580e38
Ben Raymond <ben.raymond@aad.gov.au>
2021-04-01 14:07:50 +1100

    drop aadcdb usage, remove appveyor and travis configs

--- R/fetch_data.R
+++ R/fetch_data.R
@@ -4,10 +4,10 @@
 #'
 #' The \code{format} parameter was introduced to the \code{so_isotopes} function in package version 0.4.0. The current default \code{format} is "wide", in which case the data will be formatted with one row per record and multiple measurements (of different isotopes) per row. If \code{format} is "mv", the data will be in measurement-value (long) format with multiple rows per original record, split so that each different isotope measurement appears in its own row. Note that "mv" will become the default (and possibly only) option in a later release. Currently, \code{record_id} values in measurement-value format are not unique (they follow the \code{record_id} values from the wide format). The \code{record_id} values in measurement-value format are likely to change in a future release.
 #'
-#' @param method string: "get" (fetch the data via a web GET call) or "direct" (direct database connection, for internal AAD use only)
+#' @param method string: "get" (fetch the data via a web GET call)
 #' @param cache_directory string: (optional) cache the data locally in this directory, so that they can be used offline later. Values can be "session" (a per-session temporary directory will be used, default), "persistent" (the directory returned by \code{rappdirs::user_cache_dir} will be used), or a string giving the path to the directory to use. Use \code{NULL} for no caching. An attempt will be made to create the cache directory if it does not exist. A warning will be given if a cached copy of the data file exists and is more than 30 days old. Use \code{refresh_cache = TRUE} to refresh the cached data if necessary
 #' @param refresh_cache logical: if TRUE, and data already exist in the cache_directory, they will be refreshed. If FALSE, the cached data will be used
-#' @param public_only logical: only applicable to \code{method} "direct"
+#' @param public_only logical: ignored, here for historical reasons
 #' @param verbose logical: show progress messages?
 #' @param format string: (for \code{so_isotopes} only) if "wide", the data will be formatted with one row per record and multiple measurements (of different isotopes) per row. If \code{format} is "mv", the data will be in measurement-value (long) format with multiple rows per original record, split so that each different isotope measurement appears in its own row. See Details for future changes to the default value of this parameter
 #'
@@ -92,144 +92,127 @@ get_so_data <- function(which_data, method, cache_directory, refresh_cache = FAL
     assert_that(is.string(method))
     assert_that(is.flag(refresh_cache))
     assert_that(is.flag(public_only))
-    method <- match.arg(tolower(method), c("get", "direct"))
-    if (method == "direct") {
-        if (which_data == "doi") return(so_default_doi())
-        if (!requireNamespace("aadcdb", quietly = TRUE)) {
-            stop("The aadcdb package is required for method = \"direct\"", call. = FALSE)
-        }
-        on.exit(try(aadcdb::db_close(dbh), silent = TRUE))
-        where_string <- if (public_only && !which_data %in% c("sources")) " where is_public_flag = 'Y'" else ""
-        dbh <- aadcdb::db_open()
-        x <- aadcdb::db_query(dbh, paste0("select * from ", so_opt(paste0(which_data, "_table")), where_string))
-        ## backwards compat
-        if ("geometry_point" %in% names(x)) x <- dplyr::select(x, .data$geometrypoint)
-        if ("last_modified" %in% names(x) && nrow(x)>0) x$last_modified <- ymd_hms(x$last_modified)
-        if ("taxon_group" %in% names(x) && nrow(x)>0) x <- x %>% dplyr::rename(taxon_group_soki = "taxon_group")
-        xs <- aadcdb::db_query(dbh, paste0("select * from ", so_opt("sources_table")))
-        so_set_opt(DOI = so_default_doi()) ## default DOI, since we are reading direct from DB
-    } else {
-        if (which_data == "doi") {
-            unzipped_data_dir <- soded_webget(cache_directory, refresh_cache = refresh_cache, verbose = verbose, cache_directory_only = TRUE)
-            my_data_file <- file.path(unzipped_data_dir, so_opt(paste0(which_data, "_file")))
-            ## here, if we don't have a valid DOI, return NA
-            my_doi <- if (!file.exists(my_data_file)) {
-                          NA_character_
-                      } else {
-                          tryCatch(sub("^doi:", "", gsub("[[:space:]]+", "", readLines(my_data_file, warn = FALSE)[1]), ignore.case = TRUE), error = function(e) NA_character_)
-                      }
-            return(my_doi)
-        }
-        unzipped_data_dir <- soded_webget(cache_directory, refresh_cache = refresh_cache, verbose = verbose)
-        suppress <- if (!verbose) function(...)suppressWarnings(suppressMessages(...)) else function(...) identity(...)
+    method <- match.arg(tolower(method), c("get"))
+    if (which_data == "doi") {
+        unzipped_data_dir <- soded_webget(cache_directory, refresh_cache = refresh_cache, verbose = verbose, cache_directory_only = TRUE)
         my_data_file <- file.path(unzipped_data_dir, so_opt(paste0(which_data, "_file")))
-        if (!file.exists(my_data_file)) {
-            stop("data file does not exist. Please try again using refresh_cache = TRUE. ", so_opt("issue_text"))
-        }
-        ## enforce some column formats, some of these fail the read_csv auto-detect
-        cols_fmt <- if (which_data == "sources") NULL else list(altitude_min = "d", altitude_max = "d", depth_min = "d", depth_max = "d", record_id = "d", notes = "c")
-        if (which_data %in% c("diet", "dna_diet")) {
-            cols_fmt$predator_life_stage <- "c"
-            cols_fmt$predator_sample_count <- "d"
-            cols_fmt$predator_sample_id <- "c"
-            cols_fmt$predator_size_mean <- "d"
-            cols_fmt$predator_size_min <- "d"
-            cols_fmt$predator_size_max <- "d"
-            cols_fmt$predator_size_sd <- "d"
-            cols_fmt$predator_size_units <- "c"
-            cols_fmt$predator_size_notes <- "c"
-            cols_fmt$predator_mass_mean <- "d"
-            cols_fmt$predator_mass_min <- "d"
-            cols_fmt$predator_mass_max <- "d"
-            cols_fmt$predator_mass_sd <- "d"
-            cols_fmt$predator_mass_units <- "c"
-            cols_fmt$predator_mass_notes <- "c"
-            for (wc in c("rank", "kingdom", "phylum", "class", "order", "family", "genus")) {
-                cols_fmt[[paste0("prey_worms_", wc)]] <- "c"
-                cols_fmt[[paste0("predator_worms_", wc)]] <- "c"
-            }
-        }
-        if (which_data %in% c("diet")) {
-            cols_fmt$prey_size_mean <- "d"
-            cols_fmt$prey_size_min <- "d"
-            cols_fmt$prey_size_max <- "d"
-            cols_fmt$prey_size_sd <- "d"
-            cols_fmt$prey_size_units <- "c"
-            cols_fmt$prey_size_notes <- "c"
-            cols_fmt$prey_mass_mean <- "d"
-            cols_fmt$prey_mass_min <- "d"
-            cols_fmt$prey_mass_max <- "d"
-            cols_fmt$prey_mass_sd <- "d"
-            cols_fmt$prey_mass_units <- "c"
-            cols_fmt$prey_mass_notes <- "c"
-            cols_fmt$consumption_rate_mean <- "d"
-            cols_fmt$consumption_rate_min <- "d"
-            cols_fmt$consumption_rate_max <- "d"
-            cols_fmt$consumption_rate_sd <- "d"
-        }
-        if (which_data %in% c("dna_diet")) {
-            cols_fmt$blocking_primer <- "c"
-            cols_fmt$sequence_source_id <- "c"
-            cols_fmt$other_methods_applied <- "c"
-            cols_fmt$dna_concentration <- "d"
-        }
-        if (which_data %in% c("energetics", "isotopes", "isotopes_mv", "lipids")) {
-            cols_fmt$taxon_sample_count <- "d"
-            cols_fmt$taxon_sample_id <- "c"
-            for (wc in c("rank", "kingdom", "phylum", "class", "order", "family", "genus")) {
-                cols_fmt[[paste0("taxon_worms_", wc)]] <- "c"
-            }
-        }
-        if (which_data %in% c("isotopes")) {
-            cols_fmt$taxon_size_mean <- "d"
-            cols_fmt$taxon_size_min <- "d"
-            cols_fmt$taxon_size_max <- "d"
-            cols_fmt$taxon_size_sd <- "d"
-            cols_fmt$taxon_size_units <- "c"
-            cols_fmt$taxon_mass_mean <- "d"
-            cols_fmt$taxon_mass_min <- "d"
-            cols_fmt$taxon_mass_max <- "d"
-            cols_fmt$taxon_mass_sd <- "d"
-            cols_fmt$taxon_mass_units <- "c"
-            cols_fmt$c_n_ratio_variability_value <- "d"
-            cols_fmt$c_n_ratio_variability_type <- "c"
-            cols_fmt$taxon_group_soki <- "c"
-            cols_fmt$samples_were_pooled <- "c"
-            cols_fmt$physical_sample_id <- "c"
-            cols_fmt$analytical_replicate_id <- "c"
-            cols_fmt$delta_34s_mean <- "d"
-            cols_fmt$delta_34s_variability_value <- "d"
-            cols_fmt$delta_34s_variability_type <- "c"
-        }
-        if (which_data %in% c("energetics", "isotopes_mv", "lipids")) {
-            cols_fmt$measurement_mean_value <- "d"
-            cols_fmt$measurement_min_value <- "d"
-            cols_fmt$measurement_max_value <- "d"
-            cols_fmt$measurement_variability_value <- "d"
-            cols_fmt$measurement_variability_type <- "c"
-            cols_fmt$taxon_group_soki <- "c"
-            cols_fmt$samples_were_pooled <- "c"
-            cols_fmt$physical_sample_id <- "c"
-            cols_fmt$analytical_replicate_id <- "c"
-        }
-        if (!is.null(cols_fmt)) cols_fmt <- do.call(cols, cols_fmt)
-        suppress(x <- read_csv(my_data_file, col_types = cols_fmt))
-        my_data_file <- file.path(unzipped_data_dir, so_opt("sources_file"))
-        if (!file.exists(my_data_file)) {
-            stop("sources file does not exist. Try again using refresh_cache = TRUE. ", so_opt("issue_text"))
-        }
-        suppress(xs <- read_csv(my_data_file))
-        ## DOI of the data we have just read
-        my_doi <- tryCatch({
-            my_doi_file <- file.path(unzipped_data_dir, so_opt("doi_file"))
-            if (!file.exists(my_doi_file)) {
-                so_default_doi()
-            } else {
-                gsub("[[:space:]]+", "", readLines(my_doi_file, warn = FALSE)[1])
-            }
-            }, error = function(e) so_default_doi())
-        so_set_opt(DOI = my_doi)
+        ## here, if we don't have a valid DOI, return NA
+        my_doi <- if (!file.exists(my_data_file)) {
+                      NA_character_
+                  } else {
+                      tryCatch(sub("^doi:", "", gsub("[[:space:]]+", "", readLines(my_data_file, warn = FALSE)[1]), ignore.case = TRUE), error = function(e) NA_character_)
+                  }
+        return(my_doi)
     }
+    unzipped_data_dir <- soded_webget(cache_directory, refresh_cache = refresh_cache, verbose = verbose)
+    suppress <- if (!verbose) function(...)suppressWarnings(suppressMessages(...)) else function(...) identity(...)
+    my_data_file <- file.path(unzipped_data_dir, so_opt(paste0(which_data, "_file")))
+    if (!file.exists(my_data_file)) {
+        stop("data file does not exist. Please try again using refresh_cache = TRUE. ", so_opt("issue_text"))
+    }
+    ## enforce some column formats, some of these fail the read_csv auto-detect
+    cols_fmt <- if (which_data == "sources") NULL else list(altitude_min = "d", altitude_max = "d", depth_min = "d", depth_max = "d", record_id = "d", notes = "c")
+    if (which_data %in% c("diet", "dna_diet")) {
+        cols_fmt$predator_life_stage <- "c"
+        cols_fmt$predator_sample_count <- "d"
+        cols_fmt$predator_sample_id <- "c"
+        cols_fmt$predator_size_mean <- "d"
+        cols_fmt$predator_size_min <- "d"
+        cols_fmt$predator_size_max <- "d"
+        cols_fmt$predator_size_sd <- "d"
+        cols_fmt$predator_size_units <- "c"
+        cols_fmt$predator_size_notes <- "c"
+        cols_fmt$predator_mass_mean <- "d"
+        cols_fmt$predator_mass_min <- "d"
+        cols_fmt$predator_mass_max <- "d"
+        cols_fmt$predator_mass_sd <- "d"
+        cols_fmt$predator_mass_units <- "c"
+        cols_fmt$predator_mass_notes <- "c"
+        for (wc in c("rank", "kingdom", "phylum", "class", "order", "family", "genus")) {
+            cols_fmt[[paste0("prey_worms_", wc)]] <- "c"
+            cols_fmt[[paste0("predator_worms_", wc)]] <- "c"
+        }
+    }
+    if (which_data %in% c("diet")) {
+        cols_fmt$prey_size_mean <- "d"
+        cols_fmt$prey_size_min <- "d"
+        cols_fmt$prey_size_max <- "d"
+        cols_fmt$prey_size_sd <- "d"
+        cols_fmt$prey_size_units <- "c"
+        cols_fmt$prey_size_notes <- "c"
+        cols_fmt$prey_mass_mean <- "d"
+        cols_fmt$prey_mass_min <- "d"
+        cols_fmt$prey_mass_max <- "d"
+        cols_fmt$prey_mass_sd <- "d"
+        cols_fmt$prey_mass_units <- "c"
+        cols_fmt$prey_mass_notes <- "c"
+        cols_fmt$consumption_rate_mean <- "d"
+        cols_fmt$consumption_rate_min <- "d"
+        cols_fmt$consumption_rate_max <- "d"
+        cols_fmt$consumption_rate_sd <- "d"
+    }
+    if (which_data %in% c("dna_diet")) {
+        cols_fmt$blocking_primer <- "c"
+        cols_fmt$sequence_source_id <- "c"
+        cols_fmt$other_methods_applied <- "c"
+        cols_fmt$dna_concentration <- "d"
+    }
+    if (which_data %in% c("energetics", "isotopes", "isotopes_mv", "lipids")) {
+        cols_fmt$taxon_sample_count <- "d"
+        cols_fmt$taxon_sample_id <- "c"
+        for (wc in c("rank", "kingdom", "phylum", "class", "order", "family", "genus")) {
+            cols_fmt[[paste0("taxon_worms_", wc)]] <- "c"
+        }
+    }
+    if (which_data %in% c("isotopes")) {
+        cols_fmt$taxon_size_mean <- "d"
+        cols_fmt$taxon_size_min <- "d"
+        cols_fmt$taxon_size_max <- "d"
+        cols_fmt$taxon_size_sd <- "d"
+        cols_fmt$taxon_size_units <- "c"
+        cols_fmt$taxon_mass_mean <- "d"
+        cols_fmt$taxon_mass_min <- "d"
+        cols_fmt$taxon_mass_max <- "d"
+        cols_fmt$taxon_mass_sd <- "d"
+        cols_fmt$taxon_mass_units <- "c"
+        cols_fmt$c_n_ratio_variability_value <- "d"
+        cols_fmt$c_n_ratio_variability_type <- "c"
+        cols_fmt$taxon_group_soki <- "c"
+        cols_fmt$samples_were_pooled <- "c"
+        cols_fmt$physical_sample_id <- "c"
+        cols_fmt$analytical_replicate_id <- "c"
+        cols_fmt$delta_34s_mean <- "d"
+        cols_fmt$delta_34s_variability_value <- "d"
+        cols_fmt$delta_34s_variability_type <- "c"
+    }
+    if (which_data %in% c("energetics", "isotopes_mv", "lipids")) {
+        cols_fmt$measurement_mean_value <- "d"
+        cols_fmt$measurement_min_value <- "d"
+        cols_fmt$measurement_max_value <- "d"
+        cols_fmt$measurement_variability_value <- "d"
+        cols_fmt$measurement_variability_type <- "c"
+        cols_fmt$taxon_group_soki <- "c"
+        cols_fmt$samples_were_pooled <- "c"
+        cols_fmt$physical_sample_id <- "c"
+        cols_fmt$analytical_replicate_id <- "c"
+    }
+    if (!is.null(cols_fmt)) cols_fmt <- do.call(cols, cols_fmt)
+    suppress(x <- read_csv(my_data_file, col_types = cols_fmt))
+    my_data_file <- file.path(unzipped_data_dir, so_opt("sources_file"))
+    if (!file.exists(my_data_file)) {
+        stop("sources file does not exist. Try again using refresh_cache = TRUE. ", so_opt("issue_text"))
+    }
+    suppress(xs <- read_csv(my_data_file))
+    ## DOI of the data we have just read
+    my_doi <- tryCatch({
+        my_doi_file <- file.path(unzipped_data_dir, so_opt("doi_file"))
+        if (!file.exists(my_doi_file)) {
+            so_default_doi()
+        } else {
+            gsub("[[:space:]]+", "", readLines(my_doi_file, warn = FALSE)[1])
+        }
+    }, error = function(e) so_default_doi())
+    so_set_opt(DOI = my_doi)
     if (!which_data %in% c("sources")) {
         xs <- dplyr::rename(xs, source_details = "details", source_doi = "doi")
         x <- left_join(x, dplyr::select(xs, .data$source_id, .data$source_details, .data$source_doi), by = "source_id")
